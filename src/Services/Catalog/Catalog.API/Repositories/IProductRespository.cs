135d69213e00f2a102a7b291b3c2f4cbb108cc3c
AghayevSehran <sehranagayev89@gmail.com>
2021-11-28 23:24:43 +0400

    catalog api

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRespository.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.API.Enities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRespository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+        Task CreateProduct(Product product);
+    }
+}
